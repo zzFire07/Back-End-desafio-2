974824bcae7d31f026984a2527fe13055fd3df32
BrunoCanepa <brunocanepa22@gmail.com>
2024-02-22 18:59:43 -0300

    API de gestion de industria

---
+++ src/controllers/industryController.js
@@ -0,0 +1,18 @@
+const IndustryService = require('../services/industryService.js');
+
+async function findAll(req, res) {
+    try {
+        const allIndustries = await IndustryService.findAll();
+        if (allIndustries) {
+            return res.status(200).json(allIndustries);
+        } else {
+            return res.status(204).json({ message: 'No se encontraron industrias en el sistema.' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Error al obtener las industrias', error: error.message });
+    }
+}
+
+module.exports = {
+    findAll
+};
