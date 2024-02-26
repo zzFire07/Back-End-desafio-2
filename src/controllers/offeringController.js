6871e0595b4d2e064478fb9a18d0a44b77a47fe0
Leandro <leandrodcasaretto@gmail.com>
2024-02-26 18:40:05 -0300

    Cambios

--- src/controllers/offeringController.js
+++ src/controllers/offeringController.js
@@ -1,20 +1,35 @@
-const OfferingService = require('../services/offeringService.js');
+const OfferingService = require("../services/offeringService.js");
 
 // Controller para obtener una lista con todos los offerings.
 
 async function getAll(req, res) {
   try {
-    const offerings = await OfferingService.getAll(); 
+    const offerings = await OfferingService.getAll();
     if (offerings) {
-      return res.status(200).json({ offerings: offerings, message: 'La lista de offerings ha sido obtenida correctamente.' });
+      return res
+        .status(200)
+        .json({
+          offerings: offerings,
+          message: "La lista de offerings ha sido obtenida correctamente.",
+        });
     } else {
-      return res.status(204).json({ message: 'La solicitud se ha completado con éxito pero no hay offerings en el sistema' });
+      return res
+        .status(204)
+        .json({
+          message:
+            "La solicitud se ha completado con éxito pero no hay offerings en el sistema",
+        });
     }
   } catch (error) {
-    return res.status(500).json({ message: 'Error al obtener los offerings', error: error.message });
+    return res
+      .status(500)
+      .json({
+        message: "Error al obtener los offerings",
+        error: error.message,
+      });
   }
 }
 
 module.exports = {
-  getAll
+  getAll,
 };
