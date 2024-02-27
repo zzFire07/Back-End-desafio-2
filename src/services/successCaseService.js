eb3278c48ad1a7b6b64ee13d1ae2a276854dfc49
Franco-BP <francobascialla@gmail.com>
2024-02-27 20:53:44 -0300

    Franco-BP/finalizado GetByFilter del SuccessCaseService

--- src/services/successCaseService.js
+++ src/services/successCaseService.js
@@ -1,9 +1,11 @@
+const { Op } = require('sequelize');
 const SuccessCaseModel = require('../models/successCaseModel.js');
 
 async function getByFilter(data) {
   try {
     const {client, industry, projectType, startDate, finishDate, contact} = data;
 
+    // Plantilla del Where a utilizar en el query.
     const whereFilter = {
       client: {
         [Op.eq]: client,
@@ -18,19 +20,31 @@ async function getByFilter(data) {
         [Op.gt]: startDate,
       },
       finishDate: {
-        [Op.gt]: finishDate,
+        [Op.lt]: finishDate,
       },
       contact: {
-        [Op.gt]: contact,
+        [Op.eq]: contact,
       },
     };
 
+    // Chequeo de las variables del filtro. Esto es porque Sequelize las compararía buscando un Null.
+    if (client == null) { delete whereFilter.client };
+    if (industry == null) { delete whereFilter.industry };
+    if (projectType == null) { delete whereFilter.projectType };
+    if (startDate == null) { delete whereFilter.startDate };
+    if (finishDate == null) { delete whereFilter.finishDate };
+    if (contact == null) { delete whereFilter.contact };
     
-    const cases = SuccessCaseModel.findAll(
-
-    );
-
+    // Query a la BD con los filtros.
+    const cases = SuccessCaseModel.findAll({
+      where: {...whereFilter}
+    });
+    return cases;
   } catch (error) {
     throw new Error(error);
   };
+};
+
+module.exports = {
+  getByFilter,
 };
