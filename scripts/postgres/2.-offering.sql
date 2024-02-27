b875e79f9143d8cfa54b09b59462cd71b85a2c7d
sebas-serrano <serranoavila@gmail.com>
2024-02-27 14:01:01 -0300

    script sql

---
+++ scripts/postgres/2.-offering.sql
@@ -0,0 +1,17 @@
+CREATE TABLE offering (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255)
+);
+INSERT INTO offering (id, name, description)
+VALUES
+(1, 'Offering 1', 'Description 1'),
+(2, 'Offering 2', 'Description 2'),
+(3, 'Offering 3', 'Description 3'),
+(4, 'Offering 4', 'Description 4'),
+(5, 'Offering 5', 'Description 5'),
+(6, 'Offering 6', 'Description 6'),
+(7, 'Offering 7', 'Description 7'),
+(8, 'Offering 8', 'Description 8'),
+(9, 'Offering 9', 'Description 9'),
+(10, 'Offering 10', 'Description 10');
