e57f9b48df1b8595843f6f104dbdbe980ea28ca8
BrunoCanepa <brunocanepa22@gmail.com>
2024-02-27 20:07:27 -0300

    namefix

--- src/routes/industryRoutes.js
+++ src/routes/industryRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 const IndustryController = require('../controllers/industryController');
 
 
-router.get('/industry/getAllIndustries', IndustryController.getAll);
+router.get('/industry/getAll', IndustryController.getAll);
 
  
 module.exports = router;
