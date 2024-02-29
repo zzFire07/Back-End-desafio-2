ce3b1c218d992d4de83b26712417a2ca79a5a031
Leandro <leandrodcasaretto@gmail.com>
2024-02-29 18:19:26 -0300

    cambios

--- back/src/services/clientService.js
+++ back/src/services/clientService.js
@@ -1,5 +1,5 @@
-const { or } = require('sequelize');
-const ClientModel = require('../models/clientModel.js');
+const { or } = require("sequelize");
+const ClientModel = require("../models/clientModel.js");
 
 // Servicio para crear un cliente
 async function createClient(data) {
@@ -8,38 +8,46 @@ async function createClient(data) {
 
     // Crea el cliente en la base de datos utilizando el modelo
     const newClient = await ClientModel.create({
-      name, surName, email
+      name,
+      surName,
+      email,
     });
 
     return newClient;
   } catch (error) {
-    throw new Error('Error al crear el cliente en la base de datos');
+    throw new Error("Error al crear el cliente en la base de datos");
   }
 }
 
 // Servicio para obtener un cliente por su id
 async function getByIdClient(clientId) {
   try {
-    const getClient = await ClientModel.findByPk(clientId); 
-    
+    const getClient = await ClientModel.findByPk(clientId);
+
     return getClient;
   } catch (error) {
-    throw new Error('Error al obtener el cliente con id ' + clientId + ' desde la base de datos');
+    throw new Error(
+      "Error al obtener el cliente con id " +
+        clientId +
+        " desde la base de datos"
+    );
   }
 }
 
 // Servicio para obtener todos los clientes
 async function getAllClients() {
-    try {
-      const allClients = await ClientModel.findAll({
-        order: [['id', 'ASC']], // Ordena por la columna 'id' de manera ascendente (de menor a mayor)
-      });
-      return allClients;
-    } catch (error) {
-      throw new Error('Error al obtener todos los clientes desde la base de datos');
-    }
+  try {
+    const allClients = await ClientModel.findAll({
+      order: [["id", "ASC"]], // Ordena por la columna 'id' de manera ascendente (de menor a mayor)
+    });
+    return allClients;
+  } catch (error) {
+    console.log(error);
+    throw new Error(
+      "Error al obtener todos los clientes desde la base de datos"
+    );
   }
-
+}
 
 module.exports = {
   createClient,
