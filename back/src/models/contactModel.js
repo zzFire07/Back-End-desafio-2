30634965f6449d446e06629b3f6aed3816a54716
Leandro <leandrodcasaretto@gmail.com>
2024-02-29 18:32:56 -0300

    cambios

--- back/src/models/contactModel.js
+++ back/src/models/contactModel.js
@@ -1,26 +1,26 @@
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require('../config/database.js');
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/database.js");
 
-class ContactModel extends Model { }
+class ContactModel extends Model {}
 
 ContactModel.init(
-    {
-        // Atributos del contacto (el id lo genera Postgres)
-        name: {
-            type: DataTypes.STRING,
-        },
-        lastname: {
-            type: DataTypes.STRING,
-        },
-        email: {
-            type: DataTypes.STRING,
-        },
+  {
+    // Atributos del contacto (el id lo genera Postgres)
+    name: {
+      type: DataTypes.STRING,
     },
-    {
-        sequelize,
-        modelName: 'Contact', // Nombre del modelo
-        tableName: 'contact', // Nombre de la tabla en la base de datos
-    }
+    lastname: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Contact", // Nombre del modelo
+    tableName: "contact", // Nombre de la tabla en la base de datos
+  }
 );
 
 module.exports = ContactModel;
