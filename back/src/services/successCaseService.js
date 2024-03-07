ab3d48c4bbc17c06753bec4e5cd224cf589a443d
Giorgio <Giorgio.san007@gmail.com>
2024-03-07 00:41:34 -0300

    fix

--- back/src/services/successCaseService.js
+++ back/src/services/successCaseService.js
@@ -19,7 +19,7 @@ async function createSuccessCase(data) {
     const { title, startdate, finishdate, teamsize, ispublic, industryid, clientid, projecttypeid, contactid , offeringid, casedetail, technology, 
       challenge, improvement} = data;
 
-      if (tittle == null || startdate == null || finishdate == null || teamsize == null || industryid == null || clientid == null || projecttypeid == null || contactid == null || offeringid == null || casedetail == null || technology == null || challenge == null || improvement == null) {
+      if (title == null || startdate == null || finishdate == null || teamsize == null || industryid == null || clientid == null || projecttypeid == null || contactid == null || offeringid == null || casedetail == null || technology == null || challenge == null || improvement == null) {
         throw new Error('Faltan datos obligatorios.');
       }
     
